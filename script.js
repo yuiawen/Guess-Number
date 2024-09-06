c3ef3933390aec6328684324ca3d5c0be60bf5ba
Ikhwan Alidae <126540391+yuiawen@users.noreply.github.com>
2024-09-06 08:37:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = document.querySelector('.guess').value;
+    console.log(guess, typeof guess);
+
+    // When there is no input
+    if (!guess){
+        document.querySelector('.message').textContent = 'No Number!';
+    
+        // when player wins
+    } else if (guess == secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    
+        // When guess is too high
+    } else if(guess > secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too High!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    
+        // When guess is too low
+    } else if(guess < secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too Low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
