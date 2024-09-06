c7945c28416ea75319b2d448a9b8d28448f12c0e
Ikhwan Alidae <126540391+yuiawen@users.noreply.github.com>
2024-09-06 09:16:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.querySelector('guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random()*20) + 1;
+let secretNumber = Math.trunc(Math.random()*20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = document.querySelector('.guess').value;
@@ -27,6 +27,8 @@ document.querySelector('.check').addEventListener('click', function() {
         // when player wins
     } else if (guess == secretNumber){
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
     
@@ -53,4 +55,24 @@ document.querySelector('.check').addEventListener('click', function() {
         }
     }
 });
+
+document.querySelector('.again').addEventListener('click', function(){
+    // Reset the score and secret number
+
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+
+    // Reset the message, score, and number display
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+
+  // Reset the guess input field
+  document.querySelector('.guess').value = '';
+
+  // Reset the styles
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+})
 
